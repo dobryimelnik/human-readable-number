dc2256f6f5d5b5707fac78b3766314715cc4d45b
Oleg Marjankov <marjan.om84@gmail.com>
2023-01-20 21:30:40 +0300

    feat: two-digit numbers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 module.exports = function toReadable (number) {
 
-    let arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let arr2 = ['', '', 'twelve', 'three', 'fourteen', 'fivteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let arr3 = ['', '', 'twenty', 'thirty', 'fourty', 'fivty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let arr1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let arr2 = ['', 'eleven', 'twelve', 'three', 'fourteen', 'fivteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arr3 = ['', 'ten', 'twenty', 'thirty', 'fourty', 'fivty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 
-    number = String(number);
-    return number;
+    let num = String(number);
+    let res = '';
+
+    if(num == 0) {
+        res = 'zero';
+    }
+    if(num.length == 1) {
+        res = arr1[num];
+    } else if(num.length == 2)  {
+        let x = num[1];//second digit
+        let y = num[0];//first digit
+
+        if((y == 1) && (x != 0)) {
+            res = arr2[x];
+        } else if((y == 1 ) && (x == 0)) {
+            res = arr3[y];
+        }else if(y > 1 ) {
+            res = arr3[y] + ' ' + arr1[x];
+        }
+        
+    }
+    return res;
 };
 
 
-console.log(module(3));
+console.log(module.exports(1));
  
