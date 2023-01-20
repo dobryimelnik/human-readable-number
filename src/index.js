fcfbf13f2b776a1a9b037324641c825b83b2fc62
Oleg Marjankov <marjan.om84@gmail.com>
2023-01-20 22:18:08 +0300

    three-digit numbers

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
 module.exports = function toReadable (number) {
 
     let arr1 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let arr2 = ['', 'eleven', 'twelve', 'three', 'fourteen', 'fivteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arr2 = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fivteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let arr3 = ['', 'ten', 'twenty', 'thirty', 'fourty', 'fivty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 
     let num = String(number);
     let res = '';
-
+    
     if(num == 0) {
         res = 'zero';
-    }
-    if(num.length == 1) {
+    } else if(num.length == 1) {
         res = arr1[num];
     } else if(num.length == 2)  {
-        let x = num[1];//second digit
-        let y = num[0];//first digit
+        let x = num[1];//units
+        let y = num[0];//dozens
 
-        if((y == 1) && (x != 0)) {
-            res = arr2[x];
-        } else if((y == 1 ) && (x == 0)) {
+        if(x == 0) {
             res = arr3[y];
-        }else if(y > 1 ) {
+        } else if((y == 1) && (x != 0)) {
+            res = arr2[x];
+        } else if(y > 1 ) {
             res = arr3[y] + ' ' + arr1[x];
         }
-        
+
+    } else if(num.length == 3) {
+        let x = num[2];//units
+        let y = num[1];//dozens
+        let z = num[0];//hundreds
+        let i = arr1[z] + ' hundred';// count of hundreds
+      
+        if((y == 0) && (x == 0)) {
+            res = i;
+        } else if((y == 0) && (x != 0)) {
+            res = i + ' ' + arr1[x];
+        } else if((y == 1) && (x == 0)) {
+            res = i + ' ' + arr3[y];
+        } else if((y == 1) && (x != 0)) {
+            res = i + ' ' + arr2[x];
+        } else {
+            res = i + ' ' + arr3[y] + ' ' + arr1[x];
+        }
     }
+
     return res;
 };
 
 
-console.log(module.exports(1));
+console.log(module.exports(765));
  
