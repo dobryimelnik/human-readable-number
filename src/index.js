4d733dfb3c3df09e6c9f4d69e63344f6b8dc9de2
Oleg Marjankov <marjan.om84@gmail.com>
2023-01-20 20:14:38 +0300

    feat: created arrays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
-}
+
+    let arr1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let arr2 = ['', '', 'twelve', 'three', 'fourteen', 'fivteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let arr3 = ['', '', 'twenty', 'thirty', 'fourty', 'fivty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+    number = String(number);
+    return number;
+};
+
+
+console.log(module(3));
+ 
